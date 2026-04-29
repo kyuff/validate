5a54e5e165d5ebafa49f84f6639482f641c0ecfd
kyuff <soren@sxl.dk>
2026-04-29 19:35:19 +0200

    docs(string): add godoc comments to string validators

--- string.go
+++ string.go
@@ -2,6 +2,8 @@ package validate
 
 import "regexp"
 
+// StringNotEmptyf returns a Validator that fails if value is an empty string.
+// The error message is formatted using template and args.
 func StringNotEmptyf[T ~string](value T, template string, args ...any) ValidatorFunc {
 	return func() error {
 		if value == "" {
@@ -11,10 +13,13 @@ func StringNotEmptyf[T ~string](value T, template string, args ...any) Validator
 	}
 }
 
+// StringNotEmpty returns a Validator that fails if value is an empty string.
 func StringNotEmpty[T ~string](value T) ValidatorFunc {
 	return StringNotEmptyf(value, "must not be empty")
 }
 
+// StringMinLengthf returns a Validator that fails if value has fewer than min bytes.
+// The error message is formatted using template and args.
 func StringMinLengthf[T ~string](value T, min int, template string, args ...any) ValidatorFunc {
 	return func() error {
 		if len(value) < min {
@@ -24,10 +29,13 @@ func StringMinLengthf[T ~string](value T, min int, template string, args ...any)
 	}
 }
 
+// StringMinLength returns a Validator that fails if value has fewer than min bytes.
 func StringMinLength[T ~string](value T, min int) ValidatorFunc {
 	return StringMinLengthf(value, min, "must be at least %d characters", min)
 }
 
+// StringMaxLengthf returns a Validator that fails if value exceeds max bytes.
+// The error message is formatted using template and args.
 func StringMaxLengthf[T ~string](value T, max int, template string, args ...any) ValidatorFunc {
 	return func() error {
 		if len(value) > max {
@@ -37,10 +45,13 @@ func StringMaxLengthf[T ~string](value T, max int, template string, args ...any)
 	}
 }
 
+// StringMaxLength returns a Validator that fails if value exceeds max bytes.
 func StringMaxLength[T ~string](value T, max int) ValidatorFunc {
 	return StringMaxLengthf(value, max, "must be at most %d characters", max)
 }
 
+// StringMatchesf returns a Validator that fails if value does not match pattern.
+// The error message is formatted using template and args.
 func StringMatchesf[T ~string](value T, pattern *regexp.Regexp, template string, args ...any) ValidatorFunc {
 	return func() error {
 		if !pattern.MatchString(string(value)) {
@@ -50,10 +61,13 @@ func StringMatchesf[T ~string](value T, pattern *regexp.Regexp, template string,
 	}
 }
 
+// StringMatches returns a Validator that fails if value does not match pattern.
 func StringMatches[T ~string](value T, pattern *regexp.Regexp) ValidatorFunc {
 	return StringMatchesf(value, pattern, "must match %v", pattern)
 }
 
+// StringOneOff returns a Validator that fails if value is not present in options.
+// The error message is formatted using template and args.
 func StringOneOff[T ~string](value T, options []T, template string, args ...any) ValidatorFunc {
 	return func() error {
 		for _, o := range options {
@@ -65,6 +79,7 @@ func StringOneOff[T ~string](value T, options []T, template string, args ...any)
 	}
 }
 
+// StringOneOf returns a Validator that fails if value is not present in options.
 func StringOneOf[T ~string](value T, options ...T) ValidatorFunc {
 	return StringOneOff(value, options, "%v must be one of %v", value, options)
 }
